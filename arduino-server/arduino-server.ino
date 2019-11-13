de9da063d1274c71a7a1a5d840b8041b926e29bb
armytag <armitageth@gmail.com>
2019-11-13 14:59:23 +0800

    Fixed Content-Type for SVG

--- arduino-server/arduino-server.ino
+++ arduino-server/arduino-server.ino
@@ -291,6 +291,7 @@ parseContentType(char* uri) {
          if(strcmp(ext,"htm")==0){ return "text/html"; }
     else if(strcmp(ext,"css")==0){ return "text/css"; }
     else if(strcmp(ext,"js")==0) { return "text/javascript"; }
-    else if(strcmp(ext,"jpg")==0){ return "image/*"; }
+    else if(strcmp(ext,"jpg")==0){ return "image/jpeg"; }
+    else if(strcmp(ext,"svg")==0){ return "image/svg+xml"; }
     else { return "*/*"; }
 }
